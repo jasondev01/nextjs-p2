9f26216d19000f4576725686d4a0cb805e0fe0ef
jasondev01 <jsnrbn01@gmail.com>
2023-09-10 01:18:28 +0800

    added login feature utilizing the grafbase and query using graphql

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -2,9 +2,13 @@ import { NavLinks } from '@/constants'
 import Image from 'next/image'
 import Link from 'next/link'
 import AuthProviders from './AuthProviders'
+import { getCurrentUser } from '@/lib/session'
+import ProfileMenu from './ProfileMenu'
 
-const Navbar = () => {
-    const session = {}
+const Navbar = async () => {
+    const session = await getCurrentUser()
+
+    console.log(session)
 
     return (
         <nav className="flexBetween navbar">
@@ -31,9 +35,11 @@ const Navbar = () => {
                 </ul>
             </div>
             <div className='flexCenter gap-4'>
-                {session ? (
+                {session?.user ? (
                     <>
-                        UserPhoto
+                        <ProfileMenu
+                            session={session}
+                         />
 
                         <Link href='/create-project'>
                             Share Work
